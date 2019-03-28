0477f5dfb07c8168442e2505d05b60c5d5c79278
Vida Alex <alexvida89@gmail.com>
2019-03-28 17:33:48 +0200

    seprate functions

--- www/template.js
+++ www/template.js
@@ -4,10 +4,21 @@
 var exec = cordova.require('cordova/exec'),
     utils = cordova.require('cordova/utils');
 
-module.exports = function setBeat(speed, pattern, callback) {
+// module.exports = function setBeat(speed, pattern, callback) {
+//     exec(callback, callback, 'Echo', 'setBeatSpeed', [speed,pattern]);
+//     // exec(callback, callback, 'Echo', 'sound_start ', [speed]);
+// };
+// module.exports = function setHaptic(callback) {
+//     exec(callback, callback, 'Echo', 'setHaptic');
+// };
+
+
+var myPlugin = function () {
+}
+myPlugin.setBeat = function(speed, pattern, callback) {
     exec(callback, callback, 'Echo', 'setBeatSpeed', [speed,pattern]);
-    // exec(callback, callback, 'Echo', 'sound_start ', [speed]);
-};
-module.exports = function setHaptic(callback) {
+}
+myPlugin.setHaptic = function(callback) {
     exec(callback, callback, 'Echo', 'setHaptic');
-};
+}
+modules.export = myPlugin;
