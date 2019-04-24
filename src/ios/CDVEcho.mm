bcaa4f9090d533da17d05267cac2e4d523592d7d
Vida Alex <alexvida89@gmail.com>
2019-04-24 12:34:37 +0300

    Launch

--- src/ios/CDVEcho.mm
+++ src/ios/CDVEcho.mm
@@ -55,22 +55,22 @@
     //the following block executes sound_define calls once
     static dispatch_once_t once_token;
     dispatch_once (&once_token, ^{
-        // NSURL* s = [[NSBundle mainBundle] URLForResource:@"MoreCowbell" withExtension:@"caf"];
-        // sound_define(s, 'H', 1);
-        // sound_define(s, 'L', 0.3);
-        // sound_define(s, 'S', 0.1);
-        // sound_define(s, 'N', 0);
-        // sound_define(s, 'X', 0);
-        // sound_define(s, 'M', 0.5);
-
-        NSURL* s = [[NSBundle mainBundle] URLForResource:@"Cowbell" withExtension:@"caf"];
-        sound_define(s, 'a', 1);
-        sound_define(s, 'b', 0.5);
-        sound_define(s, 'c', 0.3);
-        sound_define(s, 'd', 0.1);
-
+        NSURL* s = [[NSBundle mainBundle] URLForResource:@"Woodblock-High" withExtension:@"caf"];
+        sound_define(s, 'H', 1);
+        sound_define(s, 'L', 0.3);
+        sound_define(s, 'S', 0.1);
         sound_define(s, 'N', 0);
         sound_define(s, 'X', 0);
+        sound_define(s, 'M', 0.5);
+
+        // NSURL* s = [[NSBundle mainBundle] URLForResource:@"Cowbell" withExtension:@"caf"];
+        // sound_define(s, 'a', 1);
+        // sound_define(s, 'b', 0.5);
+        // sound_define(s, 'c', 0.3);
+        // sound_define(s, 'd', 0.1);
+
+        // sound_define(s, 'N', 0);
+        // sound_define(s, 'X', 0);
 
         // NSURL* s2 = [[NSBundle mainBundle] URLForResource:@"Woodblock-High" withExtension:@"caf"];
         // sound_define(s2, 'e', 1);
